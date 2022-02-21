916c7b96980c6501838030dd8b84b68b30956c37
alemica97 <alessandro.micalizzi08@gmail.com>
2022-02-21 16:16:49 +0100

    nel precedente ciclo for ho sostituito gli elementi dei vecchi array con le proprieta degli oggetti del nuovo array

--- js/app.js
+++ js/app.js
@@ -1,32 +1,32 @@
-//creo un array con le immagini da inserire nell'HTML
-const imgCarousel = [
-    'img/img-1.jpg',
-    'img/img-2.jpg',
-    'img/img-3.jpg',
-    'img/img-4.jpg',
-    'img/img-5.jpg',
-    'img/img-6.jpg'
-];
+// //creo un array con le immagini da inserire nell'HTML
+// const imgCarousel = [
+//     'img/img-1.jpg',
+//     'img/img-2.jpg',
+//     'img/img-3.jpg',
+//     'img/img-4.jpg',
+//     'img/img-5.jpg',
+//     'img/img-6.jpg'
+// ];
 
-//creo un array con i titoli delle immagini da inserire nell'HTML
-const imgPlaces = [
-    'Capo Milazzo',
-    'Laghetti di Marinello',
-    'Santa Tecla',
-    'Forza D\'Agrò',
-    'Capo Alì',
-    'Vulcano Etna'
-]
+// //creo un array con i titoli delle immagini da inserire nell'HTML
+// const imgPlaces = [
+//     'Capo Milazzo',
+//     'Laghetti di Marinello',
+//     'Santa Tecla',
+//     'Forza D\'Agrò',
+//     'Capo Alì',
+//     'Vulcano Etna'
+// ]
 
-//creo un array con le Descrizioni delle immagini da inserire nell'HTML
-const imgDescription = [
-    'Capo Milazzo è il capo col quale si conclude a ovest il golfo di Milazzo e inizia il golfo di Patti. Si trova nel territorio di Milazzo, comune italiano della città metropolitana di Messina.',
-    'La riserva naturale orientata Laghetti di Marinello è un\'area naturale protetta situata nel comune di Patti, in provincia di Messina. La riserva occupa oltre 400 ettari ed è stata istituita nel 1998.',
-    'Posta a nord-est di Acireale, Santa Tecla si adagia su una suggestiva piattaforma lavica: a Ovest si attacca alla lussureggiante e selvaggia Timpa "Falconiera" con la sua incolta flora mediterranea (oggi parco naturale protetto), mentre a Est si distende sul mare Ionio in una compatta e imponente scogliera di roccia vulcanica.',
-    'Forza d\'Agrò (\'a Forza in siciliano) è un comune italiano di 849 abitanti[2] della città metropolitana di Messina in Sicilia.',
-    'La spiaggia di Capo Alì si trova poco distante dal centro di Alì Terme e si raggiunge con una camminata via spiaggia di circa 20 minuti.',
-    'L\'Etna è uno stratovulcano complesso della Sicilia originatosi nel Quaternario, ed è il più alto vulcano attivo della placca euroasiatica. Le sue frequenti eruzioni nel corso del tempo hanno modificato, a volte anche profondamente, il paesaggio circostante e in tante occasioni hanno costituito una minaccia per gli insediamenti abitativi nati nel tempo alle sue pendici. Il 21 giugno 2013, la XXXVII sessione del Comitato UNESCO ha inserito l\'Etna nell\'elenco dei beni costituenti il Patrimonio dell\'umanità.'
-]
+// //creo un array con le Descrizioni delle immagini da inserire nell'HTML
+// const imgDescription = [
+//     'Capo Milazzo è il capo col quale si conclude a ovest il golfo di Milazzo e inizia il golfo di Patti. Si trova nel territorio di Milazzo, comune italiano della città metropolitana di Messina.',
+//     'La riserva naturale orientata Laghetti di Marinello è un\'area naturale protetta situata nel comune di Patti, in provincia di Messina. La riserva occupa oltre 400 ettari ed è stata istituita nel 1998.',
+//     'Posta a nord-est di Acireale, Santa Tecla si adagia su una suggestiva piattaforma lavica: a Ovest si attacca alla lussureggiante e selvaggia Timpa "Falconiera" con la sua incolta flora mediterranea (oggi parco naturale protetto), mentre a Est si distende sul mare Ionio in una compatta e imponente scogliera di roccia vulcanica.',
+//     'Forza d\'Agrò (\'a Forza in siciliano) è un comune italiano di 849 abitanti[2] della città metropolitana di Messina in Sicilia.',
+//     'La spiaggia di Capo Alì si trova poco distante dal centro di Alì Terme e si raggiunge con una camminata via spiaggia di circa 20 minuti.',
+//     'L\'Etna è uno stratovulcano complesso della Sicilia originatosi nel Quaternario, ed è il più alto vulcano attivo della placca euroasiatica. Le sue frequenti eruzioni nel corso del tempo hanno modificato, a volte anche profondamente, il paesaggio circostante e in tante occasioni hanno costituito una minaccia per gli insediamenti abitativi nati nel tempo alle sue pendici. Il 21 giugno 2013, la XXXVII sessione del Comitato UNESCO ha inserito l\'Etna nell\'elenco dei beni costituenti il Patrimonio dell\'umanità.'
+// ]
 
 //Trasformo i tre array imgCarousel, imgPlaces ed imgDescription in un unico array composto da oggetti
 const carouselObjects = [
@@ -79,25 +79,31 @@ console.log(prevImg);
 //creo una variabile che userò dopo per gestire la classe active
 let activeClass = 0;
 
-for( let i = 0; i < imgCarousel.length; i++ ){
+for( let i = 0; i < carouselObjects.length; i++ ){
     // console.log(imgCarousel[i], imgPlaces[i]);
+    const image = carouselObjects[i].image;
+    // console.log(image);
+    const place = carouselObjects[i].place;
+    // console.log(place);
+    const description = carouselObjects[i].description;
+    // console.log(description);
     //Creo un div "item" che conterrà un'immagine e un titolo/descrizione
     const item = document.createElement('div');
     item.classList.add('item');
     slidesWrapper.append(item);
 
     //Creo un tag img e un h1 che unisco al div "item" nell'HTML
-    const imgItem = `<img src="${imgCarousel[i]}">`;
+    const imgItem = `<img src="${image}">`;
     item.innerHTML += imgItem;
 
-    const imgTitle = `<h1 class="title">${imgPlaces[i]}</h1>`;
+    const imgTitle = `<h1 class="title">${place}</h1>`;
     item.innerHTML += imgTitle;
 
-    const imgParagraph = `<p class="description">${imgDescription[i]}`
+    const imgParagraph = `<p class="description">${description}`
     item.innerHTML += imgParagraph;
 
     const controlWrapper = `<div class="control-wrapper">
-                                <img src="${imgCarousel[i]}">
+                                <img src="${image}">
                             </div>`;
     imgPreview.innerHTML += controlWrapper;
     
